b6fcdb04f91df37d9c9f4ff613ad69d22507f545
JoelAZ <joelzorri@gmail.com>
2025-05-14 21:06:59 -0300

    add header

--- app.vue
+++ app.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-import "@/assets/css/tailwind.css";
-import "vue-toastification/dist/index.css";
+	import "@/assets/css/tailwind.css";
+	import "vue-toastification/dist/index.css";
+	import { Header } from "./components/Header.vue";
 </script>
+
 <template>
-  <div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
+	<div>
+		<NuxtLayout>
+			<!-- Invoca el componente, no la etiqueta HTML -->
+			<Header />
+			<NuxtPage />
+		</NuxtLayout>
+	</div>
 </template>
