5fca16c750911b19b40fc3c7199e561f8feb0f6c
hakapichona <zetagameplay25@outlook.es>
2025-06-26 23:34:16 -0300

    ecosystem.config.js upgraded

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,5 +2,9 @@ export const apps = [
   {
     name: "prestigio-web",
     script: ".output/server/index.mjs",
+    env: {
+      NITRO_PORT: process.env.NITRO_PORT,
+      NUXT_PUBLIC_API_BASE_URL: process.env.NITRO_PUBLIC_API_URL,
+    },
   },
 ];
